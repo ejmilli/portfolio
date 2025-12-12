cd89cc0639fe5c2738c3eb11a3a8ffd0020ee331
sfasasa <ejmilli03@gmail.com>
2025-12-13 00:37:37 +0200

    restarting my new portfolio that im passionate about

--- Desktop/portfolio/react-portfolio/src/utils.js
+++
@@ -1,5 +0,0 @@
-// using this path makes it easier to dynamically import images into our diff components
-
-export const getImageUrl = (path) => {
-  return new URL(`/assets/${path}`, import.meta.url).href;
-};
